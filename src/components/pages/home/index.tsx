af28920828b458db5bd10df734b07dd84e6de943
Shiddhartha-Pal <pal35-1069@diu.edu.bd>
2025-04-13 21:36:07 +0600

    Add Profile button to ContestTrackerHome component

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -163,6 +163,9 @@ export default function ContestTrackerHome() {
               >
                 Log out
               </button>
+              <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left">
+                Profile
+              </button>
             </div>
           )}
         </div>
